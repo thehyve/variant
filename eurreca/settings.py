90b8eddadcf906948bb7c79219540a7f02e5bad7
Roland van Laar <roland@micite.net>
2011-09-29 14:37:09 +0200

    Added django-extensions. Django extension adds usefull management commands.

--- eurreca/settings.py
+++ eurreca/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
+    'django_extensions',
     # 'django.contrib.admindocs',
 )
 
