99ce0878b4e949893b46bb0b4a2e0fd3b01bb0b3
Taco Steemers <taco@thehyve.nl>
2011-12-06 12:43:21 +0100

    Part of VAR-69 and VAR-85 - a change to the search layout.

--- eurreca/templatetags/truncatechars.py
+++ eurreca/templatetags/truncatechars.py
@@ -32,3 +32,31 @@ def truncatechars(value, length):
             return value[:length]
     else:
         return value
+
+@register.filter()
+def truncatewords(value, length):
+    """
+        truncate after a certain number of characters,
+        if the last character is not space truncate at the next space
+        
+        Edit by Taco: Now adds three dots at the end, and takes those dots
+            in to account when checking where to truncate and add the dots.
+    """
+    if value == None or value == '':
+        return value
+    value_array = []
+    try:
+        value_array = str(value).lsplit(' ')
+    except Excption:
+        return value
+    
+    try:
+        return_value  = value_array[0]
+        le = 0
+        while le < length:
+            return_value += value_array[le]
+            le += 1
+        return_value += '...'
+        return return_value
+    except IndexError:
+        return value
