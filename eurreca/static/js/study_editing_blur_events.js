b80dfc48dbec6eb9dafbf78989021250bce78b5d
Taco Steemers <taco@thehyve.nl>
2011-12-20 13:24:11 +0100

    Fixed VAR-34 pubmed idea is set to -1 with large numbers

---
+++ eurreca/static/js/study_editing_blur_events.js
@@ -0,0 +1,9 @@
+﻿function initBlurEvents(){
+    $('input[id=id_study-0-pubmed_id]').blur(function() {
+        var value = $(this).val();
+        if(value.length>10 || !value.match(/^\d{1,10}$/ )){
+            message1 = 'Pubmed id can exist of at most ten digits.'
+            alert(message1);
+        }
+    });
+}
