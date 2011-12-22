79c62fcc3921d1b98509c3fc209e4b2c2dfe5617
Robert Horlings <robert@isdat.nl>
2011-12-22 10:03:41 +0100

    Now a better merge

--- eurreca/models.py
+++ eurreca/models.py
@@ -8,27 +8,27 @@ class Study(models.Model):
         ('U', 'Unknown'),
     )
     study_id = models.CharField(max_length=50)
-    pubmed_id = models.IntegerField(max_length=9)
+    pubmed_id = models.IntegerField(max_length=10)
     year_of_publication = models.CharField(max_length=4)
-    authors = models.CharField(max_length=200)
-    micronutrient = models.CharField(max_length=200, blank=True)
-    population = models.CharField(max_length=200, blank=True)
+    authors = models.CharField(max_length=500)
+    micronutrient = models.CharField(max_length=500, blank=True)
+    population = models.CharField(max_length=500, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
-    endpoint = models.CharField(max_length=200, blank=True)
-    paper_title = models.CharField(max_length=200)
-    journal_title = models.CharField(max_length=200, blank=True)
-    study_type = models.CharField(max_length=200, blank=True)
-    number_of_participants = models.IntegerField(max_length=9, blank=True, null=True)
+    endpoint = models.CharField(max_length=500, blank=True)
+    paper_title = models.CharField(max_length=500)
+    journal_title = models.CharField(max_length=500, blank=True)
+    study_type = models.CharField(max_length=500, blank=True)
+    number_of_participants = models.CharField(max_length=50, blank=True, null=True)
     comments = models.CharField(max_length=500, blank=True, null=True)
-    environmental_factor = models.CharField(max_length=200, blank=True, null=True)
+    environmental_factor = models.CharField(max_length=500, blank=True, null=True)
     
     def __unicode__(self):
         return self.study_id
                 
 class Phenotype(models.Model):
     study = models.ForeignKey(Study)
-    phenotype_name = models.CharField(max_length=200)
-    intake_data = models.CharField(max_length=200, blank=True)
+    phenotype_name = models.CharField(max_length=500)
+    intake_data = models.CharField(max_length=500, blank=True)
     
     def __unicode__(self):
         return "{0}".format(self.phenotype_name)
@@ -41,17 +41,17 @@ class Genotype(models.Model):
         ('U', 'Unknown'),
     )
     study = models.ForeignKey(Study)
-    gene = models.CharField(max_length=200)
-    snp_ref = models.CharField(max_length=200, blank=True, null=True)
-    snp_variant = models.CharField(max_length=200, blank=True, null=True)
-    snp_name = models.CharField(max_length=200, blank=True, null=True)
-    allele = models.CharField(max_length=200, blank=True)
-    mutation = models.CharField(max_length=200, blank=True)
+    gene = models.CharField(max_length=500)
+    snp_ref = models.CharField(max_length=500, blank=True, null=True)
+    snp_variant = models.CharField(max_length=500, blank=True, null=True)
+    snp_name = models.CharField(max_length=500, blank=True, null=True)
+    allele = models.CharField(max_length=500, blank=True)
+    mutation = models.CharField(max_length=500, blank=True)
     zygosity = models.CharField(max_length=1, choices=TYPE_CHOICES, blank=True)
-    number_of_people_with_genotype = models.IntegerField(max_length=9, blank=True, null=True)
-    genotype_frequency = models.CharField(max_length=30, null=True, blank=True)
+    number_of_people_with_genotype = models.CharField(max_length=50, blank=True, null=True)
+    genotype_frequency = models.CharField(max_length=500, null=True, blank=True)
     estimated_overal_frequency = models.CharField(max_length=10, null=True, blank=True)
-    genotype_details = models.CharField(max_length=200, blank=True)
+    genotype_details = models.CharField(max_length=500, blank=True)
     
     def __unicode__(self):
         # Order of preference:
@@ -71,10 +71,10 @@ class Panel(models.Model):
     )
     study = models.ForeignKey(Study)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
-    number_of_participants = models.IntegerField(max_length=9, blank=True, null=True)
+    number_of_participants = models.CharField(max_length=50, blank=True, null=True)
     mean_age = models.CharField(max_length=10, null=True, blank=True)
-    additional_age_description = models.CharField(max_length=200, blank=True)
-    panel_description = models.CharField(max_length=200)
+    additional_age_description = models.CharField(max_length=500, blank=True)
+    panel_description = models.CharField(max_length=500)
     
 class Interaction(models.Model):
     RATIO_TYPE_CHOICES = (
@@ -86,7 +86,7 @@ class Interaction(models.Model):
     genotypes = models.ManyToManyField(Genotype, null=True, blank=True)
     phenotypes = models.ManyToManyField(Phenotype, null=True, blank=True)
     panels = models.ManyToManyField(Panel, null=True, blank=True)
-    statistical_model = models.CharField(max_length=200, blank=True)
+    statistical_model = models.CharField(max_length=500, blank=True)
     p_value = models.CharField(max_length=10, null=True, blank=True)
     p_value_for_trend = models.CharField(max_length=10, null=True, blank=True)
     ratio_type = models.CharField(max_length=1, choices = RATIO_TYPE_CHOICES, blank=True)
@@ -94,8 +94,8 @@ class Interaction(models.Model):
     ci_lower = models.CharField(max_length=10, null=True, blank=True)
     ci_upper = models.CharField(max_length=10, null=True, blank=True)
     significant_associations = models.CharField(max_length=500, blank=True)
-    endpoint = models.CharField(max_length=200, blank=True)
+    endpoint = models.CharField(max_length=500, blank=True)
     
 class Link_to_dbSNP(models.Model):
-    snp_ref = models.CharField(max_length=200)
+    snp_ref = models.CharField(max_length=500)
     url = models.CharField(max_length=300)
